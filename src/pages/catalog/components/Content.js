af4046578efab195e2eb16f67674208a85bd1459
Michael <michael.gamel@hotmail.com>
2019-07-21 01:22:24 +0400

    add not found products placeholder

--- src/pages/catalog/components/Content.js
+++ src/pages/catalog/components/Content.js
@@ -16,19 +16,32 @@ export default ({ data }) => {
     setShow(false);
   };
 
-  const cards = data.map(item => (
-    <div className="col-3 mb-3" key={item.id}>
-      <Card
-        item={item}
-        showDetails={ItemDetails => showDetailsHandler(ItemDetails)}
-      />
-    </div>
-  ));
+  const cards =
+    data.length > 0 ? (
+      data.map(item => (
+        <div className="col-3 mb-3" key={item.id}>
+          <Card
+            item={item}
+            showDetails={ItemDetails => showDetailsHandler(ItemDetails)}
+          />
+        </div>
+      ))
+    ) : (
+      <div className="data-not-found">
+        <img src="https://k.nooncdn.com/s/app/2019/com-www-bigalog/1800fd3741eeb9b8e17ff17f6e9cafe93e537f30/static/images/empty-state-product.png" />
+        <h3> We couldn’t find what you were looking for </h3>
+        <p>Keep calm and search again. We have SO many other products that you will like!</p>
+      </div>
+    );
 
   return (
     <>
       <div className="row">{cards}</div>
-      <Modal isVisable={show} title={details.phone} onClose={() => onModalCloseHandler()}>
+      <Modal
+        isVisable={show}
+        title={details.phone}
+        onClose={() => onModalCloseHandler()}
+      >
         <CardDetails item={details} />
       </Modal>
     </>
