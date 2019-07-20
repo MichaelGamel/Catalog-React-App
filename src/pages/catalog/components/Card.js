9f9e4abae67867dd181a479b240cca3d2573dcc0
Michael <michael.gamel@hotmail.com>
2019-07-20 11:37:50 +0400

    improve the performance by lazy load the images

--- src/pages/catalog/components/Card.js
+++ src/pages/catalog/components/Card.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import LazyLoad from 'react-lazy-load';
 
 export default ({ item, showDetails }) => {
   return (
     <div className="card">
-      <img className="card-img-top" src={item.picture} alt="Card cap" />
+      <LazyLoad debounce={false} offsetVertical={500}>
+        <img className="card-img-top" src={item.picture} alt="{item.phone}" />
+      </LazyLoad>
       <div className="card-body">
         <h5 className="card-text">
           <small> {item.brand} </small>
         </h5>
         <p className="card-title">{item.phone}</p>
       </div>
-        <p className="card-price">AED {item.release.priceEur}</p>
-        <button onClick={() => showDetails(item)} className="btn btn-primary">
-          View Details
-        </button>
+      <p className="card-price">AED {item.release.priceEur}</p>
+      <button onClick={() => showDetails(item)} className="btn btn-primary">
+        View Details
+      </button>
     </div>
   );
 };
